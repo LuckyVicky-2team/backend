18ae2964bc19a94bfc5d679957acd0e3ebc65b15
ambosing <ambosing_@naver.com>
2024-08-04 11:04:07 +0900

    Feat: JWT 토큰 발급 구현

---
+++ src/main/java/com/boardgo/jwt/JWTUtil.java
@@ -0,0 +1,67 @@
+package com.boardgo.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JWTUtil {
+	private SecretKey secretKey;
+
+	public JWTUtil(@Value("${spring.jwt.secret}") String secretKey) {
+		this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8),
+			Jwts.SIG.HS256.key().build().getAlgorithm());
+	}
+
+	public String getEmail(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("email", String.class);
+	}
+
+	public String getRole(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("role", String.class);
+	}
+
+	public Boolean isExpired(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.getExpiration()
+			.before(new Date());
+	}
+
+	public String createJwt(String email, String role, Long expiredSecond) {
+
+		Date issuedAt = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
+		Date expiration = Date.from(
+			LocalDateTime.now().plusSeconds(expiredSecond).atZone(ZoneId.systemDefault()).toInstant());
+
+		return Jwts.builder()
+			.claim("email", email)
+			.claim("role", role)
+			.issuedAt(issuedAt)
+			.expiration(expiration)
+			.signWith(secretKey)
+			.compact();
+	}
+}
