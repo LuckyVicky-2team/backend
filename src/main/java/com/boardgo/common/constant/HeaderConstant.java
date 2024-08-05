495ea373676e39f98789c89c703f2cbcc319c7fa
ambosing <ambosing_@naver.com>
2024-08-05 21:00:36 +0900

    Fix: 사용하지 않는 상수 제거
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/main/java/com/boardgo/common/constant/HeaderConstant.java
+++ src/main/java/com/boardgo/common/constant/HeaderConstant.java
@@ -1,17 +1,9 @@
 package com.boardgo.common.constant;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public class HeaderConstant {
 
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
     public static final String API_VERSION_HEADER = "X-API-Version";
     public static final String API_VERSION_HEADER1 = "X-API-Version=1";
-
-    @Value("${spring.dev.host}")
-    public static String HOST;
-
-    @Value("${spring.dev.port}")
-    public static String PORT;
 }
