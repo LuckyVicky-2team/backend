7139eadbb4e1f5b735eb4dd285ef853db25323fc
ambosing <ambosing_@naver.com>
2024-08-06 23:01:25 +0900

    Feat: Custom Validation 추가
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/main/java/com/boardgo/domain/meeting/controller/dto/MeetingCreateRequest.java
+++ src/main/java/com/boardgo/domain/meeting/controller/dto/MeetingCreateRequest.java
@@ -1,23 +1,20 @@
 package com.boardgo.domain.meeting.controller.dto;
 
+import com.boardgo.common.validator.annotation.AllowedValues;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-
 public record MeetingCreateRequest(
-	@NotEmpty(message = "content") String content,
-	@NotEmpty(message = "type") String type,
-	String thumbnail,
-	String city,
-	String county,
-	@NotNull @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	LocalDateTime meetingDatetime,
-	@NotNull @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	LocalDateTime endDatetime,
-	List<Long> boardGameIdList
-) {
-}
+        @NotEmpty(message = "content") String content,
+        @NotEmpty(message = "type") @AllowedValues(values = {"Free", "ACCEPT"}) String type,
+        @Positive Integer limitParticipant,
+        String city,
+        String county,
+        @Future @NotNull @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime meetingDatetime,
+        @Future @NotNull @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime endDatetime,
+        List<Long> boardGameIdList) {}
