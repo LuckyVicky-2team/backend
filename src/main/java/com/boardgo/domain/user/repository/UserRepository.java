fd844e2ce7cbfccddfcd15543e5dbd9c2bb13ed4
ambosing <ambosing_@naver.com>
2024-08-06 15:22:29 +0900

    Fix: 로그인 부분 문제 해결
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/main/java/com/boardgo/domain/user/repository/UserRepository.java
+++ src/main/java/com/boardgo/domain/user/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.boardgo.domain.user.repository;
 
 import com.boardgo.domain.user.entity.UserInfoEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserInfoEntity, Long> {
 
-    UserInfoEntity findByEmail(String email);
+    Optional<UserInfoEntity> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
--- src/main/java/com/boardgo/domain/user/service/CustomUserDetailsService.java
+++ src/main/java/com/boardgo/domain/user/service/CustomUserDetailsService.java
@@ -5,11 +5,13 @@ import com.boardgo.domain.user.repository.UserRepository;
 import com.boardgo.domain.user.service.dto.CustomUserDetails;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
@@ -17,8 +19,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
-        Optional<UserInfoEntity> userInfoEntity = userRepository.findById(Long.valueOf(id));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<UserInfoEntity> userInfoEntity = userRepository.findByEmail(email);
         return new CustomUserDetails(userInfoEntity.orElseThrow());
     }
 }
