6c6fb6a16e28f72fc9352cc61f6665dae4e00b2a
yeahdy <yejine98@naver.com>
2024-08-08 16:17:06 +0900

    Fix: 회원가입 ProviderType Mapstruct 매핑 수정

--- src/main/java/com/boardgo/domain/mapper/UserInfoMapper.java
+++ src/main/java/com/boardgo/domain/mapper/UserInfoMapper.java
@@ -4,12 +4,17 @@ import com.boardgo.domain.user.controller.dto.SignupRequest;
 import com.boardgo.domain.user.entity.UserInfoEntity;
 import com.boardgo.oauth2.dto.OAuth2CreateUserRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserInfoMapper {
     UserInfoMapper INSTANCE = Mappers.getMapper(UserInfoMapper.class);
 
+    @Mapping(
+            source = "providerType",
+            target = "providerType",
+            defaultExpression = "java(com.boardgo.domain.user.entity.ProviderType.LOCAL)")
     UserInfoEntity toUserInfoEntity(SignupRequest signupRequest);
 
     UserInfoEntity toUserInfoEntity(OAuth2CreateUserRequest oAuth2CreateUserRequest);
