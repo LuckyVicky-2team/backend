02e383b518ca5fb89e1649cc045c554ce12a1797
yeahdy <yejine98@naver.com>
2024-08-09 19:52:10 +0900

    Fix: 토큰 만료시간 수정

--- src/main/java/com/boardgo/common/constant/TimeConstant.java
+++ src/main/java/com/boardgo/common/constant/TimeConstant.java
@@ -2,5 +2,5 @@ package com.boardgo.common.constant;
 
 public class TimeConstant {
     // 단위 Minutes -> 15분
-    public static Long ACCESS_TOKEN_DURATION = 15L;
+    public static Long ACCESS_TOKEN_DURATION = 30L;
 }
