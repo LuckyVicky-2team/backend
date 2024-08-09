87d2f4ffc8b4f24b66e2457db854ecdbc8a930a3
ambosing <ambosing_@naver.com>
2024-08-09 21:34:11 +0900

    Fix: RestAssured 설정 변경
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/test/java/com/boardgo/integration/support/RestDocsTestSupport.java
+++ src/test/java/com/boardgo/integration/support/RestDocsTestSupport.java
@@ -3,23 +3,20 @@ package com.boardgo.integration.support;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.*;
 
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @ExtendWith(RestDocumentationExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 @ActiveProfiles("test")
 public abstract class RestDocsTestSupport {
     @Value("${spring.jwt.test-token}")
@@ -43,5 +40,6 @@ public abstract class RestDocsTestSupport {
                                                 prettyPrint())
                                         .withResponseDefaults(prettyPrint()))
                         .build();
+        RestAssured.port = port; // RestAssured에 포트를 설정
     }
 }
