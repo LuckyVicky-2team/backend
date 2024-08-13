ef00e51167019d5c0797680baf48bfefa7487ae6
ambosing <ambosing_@naver.com>
2024-08-13 12:28:39 +0900

    Feat: 보드게임 추가 문서화 및 컨트롤러 추가
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/main/java/com/boardgo/domain/boardgame/controller/request/BoardGameCreateRequest.java
+++ src/main/java/com/boardgo/domain/boardgame/controller/request/BoardGameCreateRequest.java
@@ -1,11 +1,15 @@
 package com.boardgo.domain.boardgame.controller.request;
 
+import com.boardgo.common.validator.annotation.ListInStringNotEmpty;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 public record BoardGameCreateRequest(
-        String title,
-        Integer minPeople,
-        Integer maxPeople,
-        Integer minPlaytime,
-        Integer maxPlaytime,
-        List<String> genres) {}
+        @NotEmpty String title,
+        @PositiveOrZero Integer minPeople,
+        @PositiveOrZero Integer maxPeople,
+        @Positive Integer minPlaytime,
+        @Positive Integer maxPlaytime,
+        @ListInStringNotEmpty List<String> genres) {}
--- src/test/java/com/boardgo/integration/boardgame/service/BoardGameCommandServiceV1Test.java
+++ src/test/java/com/boardgo/integration/boardgame/service/BoardGameCommandServiceV1Test.java
@@ -1,4 +1,4 @@
-package com.boardgo.integration.boardgame;
+package com.boardgo.integration.boardgame.service;
 
 import static org.assertj.core.api.Assertions.*;
 
