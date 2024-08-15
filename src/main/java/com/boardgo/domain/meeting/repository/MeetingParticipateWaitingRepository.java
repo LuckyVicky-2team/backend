5c2d8d2734df8c2055bf8369f22841a6ca78d735
yeahdy <yejine98@naver.com>
2024-08-15 19:31:38 +0900

    Feat: 모임 참가자 대기 엔티티 및 레포지토리 생성
    - MeetingParticipateWaitingEntity 생성
    - MeetingParticipantSubEntity participantCount 필드 타입 변경

---
+++ src/main/java/com/boardgo/domain/meeting/repository/MeetingParticipateWaitingRepository.java
@@ -0,0 +1,9 @@
+package com.boardgo.domain.meeting.repository;
+
+import com.boardgo.domain.meeting.entity.MeetingParticipateWaitingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeetingParticipateWaitingRepository
+        extends JpaRepository<MeetingParticipateWaitingEntity, Long> {
+    boolean existsByUserInfoId(Long userId);
+}
