e8d2007acd52311da3f75af85b68747ee61f6a4e
yeahdy <yejine98@naver.com>
2024-08-23 15:37:07 +0900

    Refactor: 상황별 추천 인원 enum 상수화

--- src/main/java/com/boardgo/domain/home/enums/SituationType.java
+++ src/main/java/com/boardgo/domain/home/enums/SituationType.java
@@ -6,10 +6,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SituationType {
-    TWO("2인 게임"),
-    THREE("3인 게임"),
-    MANY("다인원"),
-    ALL("전체 장르");
+    TWO("2인 게임", 2),
+    THREE("3인 게임", 3),
+    MANY("다인원", 4),
+    ALL("전체 장르", 0);
 
     private String name;
+    private int people;
 }
--- src/main/java/com/boardgo/domain/home/service/HomeQueryServiceV1.java
+++ src/main/java/com/boardgo/domain/home/service/HomeQueryServiceV1.java
@@ -1,8 +1,6 @@
 package com.boardgo.domain.home.service;
 
-import com.boardgo.domain.boardgame.repository.BoardGameGenreRepository;
 import com.boardgo.domain.boardgame.repository.BoardGameRepository;
-import com.boardgo.domain.boardgame.repository.GameGenreMatchRepository;
 import com.boardgo.domain.boardgame.repository.dto.SituationBoardGameDto;
 import com.boardgo.domain.home.controller.request.SituationRequest;
 import com.boardgo.domain.home.controller.response.SituationBoardGameResponse;
@@ -17,32 +15,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class HomeQueryService implements HomeQueryUseCase {
+public class HomeQueryServiceV1 implements HomeQueryUseCase {
 
     private final BoardGameRepository boardGameRepository;
     private final HomeMapper homeMapper;
-    private final BoardGameGenreRepository boardGameGenreRepository;
-    private final GameGenreMatchRepository gameGenreMatchRepository;
 
     @Override
     public List<SituationBoardGameResponse> getSituationBoardGame(
             SituationRequest situationRequest) {
         SituationType situationType = SituationType.valueOf(situationRequest.situationType());
-        List<SituationBoardGameDto> situationBoardGames = null;
-        switch (situationType) {
-            case TWO -> {
-                situationBoardGames = boardGameRepository.findByMaxPeopleBetween(2);
-            }
-            case THREE -> {
-                situationBoardGames = boardGameRepository.findByMaxPeopleBetween(3);
-            }
-            case MANY -> {
-                situationBoardGames = boardGameRepository.findByMaxPeopleBetween(4);
-            }
-            case ALL -> {
-                situationBoardGames = boardGameRepository.findByMaxPeopleBetween(0);
-            }
-        }
+        List<SituationBoardGameDto> situationBoardGames =
+                boardGameRepository.findByMaxPeopleBetween(situationType.getPeople());
 
         Map<String, SituationBoardGameResponse> boardGameMap = new HashMap<>();
         for (SituationBoardGameDto gameDto : situationBoardGames) {
