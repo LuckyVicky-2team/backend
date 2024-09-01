351f271798acf5361fd40a3f204c7440b11c2b68
yeahdy <yejine98@naver.com>
2024-09-01 21:22:54 +0900

    Fix: SchedulerFactoryBean 설정 매개변수로 변경 및 sql log 라이브러리 추가

--- src/main/java/com/boardgo/schedule/config/QuartzConfig.java
+++ src/main/java/com/boardgo/schedule/config/QuartzConfig.java
@@ -1,13 +1,12 @@
 package com.boardgo.schedule.config;
 
-import java.io.IOException;
 import java.util.Properties;
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 @Configuration
@@ -19,13 +18,19 @@ public class QuartzConfig {
     private final ApplicationContext applicationContext;
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean() {
+    public SchedulerFactoryBean schedulerFactoryBean(
+            DataSource dataSource, QuartzProperties quartzProperties) {
         SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+        // yml
+        schedulerFactory.setDataSource(dataSource);
+        Properties properties = new Properties();
+        properties.putAll(quartzProperties.getProperties());
+        schedulerFactory.setQuartzProperties(properties);
+
         // application start > scheduler auto start after 30s
         schedulerFactory.setAutoStartup(true);
         schedulerFactory.setStartupDelay(3);
 
-        schedulerFactory.setQuartzProperties(quartzProperties());
         schedulerFactory.setGlobalJobListeners(jobsListener);
         schedulerFactory.setGlobalTriggerListeners(triggersListener);
         schedulerFactory.setOverwriteExistingJobs(true);
@@ -37,18 +42,5 @@ public class QuartzConfig {
         schedulerFactory.setJobFactory(jobFactory);
 
         return schedulerFactory;
-    }
-
-    private Properties quartzProperties() {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
-        Properties properties;
-        try {
-            propertiesFactoryBean.afterPropertiesSet();
-            properties = propertiesFactoryBean.getObject();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return properties;
     }
 }
