d66b38d4c29322fcc2df9a23ad785c9340dd46d4
yeahdy <yejine98@naver.com>
2024-09-04 15:48:35 +0900

    Chore: ReviewUseCase > ReviewQueryUseCase 클래스명 변경

--- src/test/java/com/boardgo/integration/review/service/ReviewQueryServiceV1Test.java
+++ src/test/java/com/boardgo/integration/review/service/ReviewQueryServiceV1Test.java
@@ -1,9 +1,26 @@
 package com.boardgo.integration.review.service;
 
+import com.boardgo.domain.review.service.ReviewQueryUseCase;
 import com.boardgo.integration.support.IntegrationTestSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ReviewQueryServiceV1Test extends IntegrationTestSupport {
-    // TODO 강퇴된 모임은 리뷰 모임 목록에 존재할 수 없다
+
+    @Autowired private ReviewQueryUseCase reviewQueryUseCase;
+
+    @Test
+    @DisplayName("강퇴된 모임은 리뷰 모임 목록에 존재할 수 없다")
+    void 강퇴된_모임은_리뷰_모임_목록에_존재할_수_없다() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
     // TODO 종료된 모임만 리뷰 모임 목록에 존재한다
     // TODO 모임에 참가한 참여자에게 모두 리뷰를 작성했을 경우 작성할 리뷰 모임 목록에 존재하지 않는다
 
