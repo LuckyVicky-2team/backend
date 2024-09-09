d497f37afa95a1167f8206a27a398edc987970c0
ambosing <ambosing_@naver.com>
2024-09-09 15:26:15 +0900

    Feat: 임시 리프레시 토큰 추가
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/main/java/com/boardgo/domain/user/service/UserQueryServiceV1.java
+++ src/main/java/com/boardgo/domain/user/service/UserQueryServiceV1.java
@@ -1,17 +1,17 @@
 package com.boardgo.domain.user.service;
 
-import static com.boardgo.common.exception.advice.dto.ErrorCode.DUPLICATE_DATA;
-import static com.boardgo.domain.meeting.entity.enums.ParticipantType.LEADER;
-import static com.boardgo.domain.meeting.entity.enums.ParticipantType.PARTICIPANT;
+import static com.boardgo.common.exception.advice.dto.ErrorCode.*;
+import static com.boardgo.domain.meeting.entity.enums.ParticipantType.*;
 
 import com.boardgo.common.exception.CustomIllegalArgumentException;
+import com.boardgo.common.exception.CustomNullPointException;
 import com.boardgo.domain.mapper.UserInfoMapper;
 import com.boardgo.domain.meeting.repository.MeetingParticipantRepository;
 import com.boardgo.domain.review.repository.ReviewRepository;
 import com.boardgo.domain.user.controller.request.EmailRequest;
 import com.boardgo.domain.user.controller.request.NickNameRequest;
+import com.boardgo.domain.user.entity.UserInfoEntity;
 import com.boardgo.domain.user.entity.enums.ProviderType;
-import com.boardgo.domain.user.repository.UserPrTagRepository;
 import com.boardgo.domain.user.repository.UserRepository;
 import com.boardgo.domain.user.repository.projection.PersonalInfoProjection;
 import com.boardgo.domain.user.service.response.OtherPersonalInfoResponse;
@@ -26,10 +26,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserQueryServiceV1 implements UserQueryUseCase {
     private final UserRepository userRepository;
-    private final UserPrTagRepository userPrTagRepository;
     private final MeetingParticipantRepository meetingParticipantRepository;
     private final UserInfoMapper UserInfoMapper;
     private final ReviewRepository reviewRepository;
+
+    @Override
+    public UserInfoEntity getById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new CustomNullPointException("유저"));
+    }
 
     @Override
     public void existEmail(EmailRequest emailRequest) {
--- src/main/java/com/boardgo/domain/user/service/UserQueryUseCase.java
+++ src/main/java/com/boardgo/domain/user/service/UserQueryUseCase.java
@@ -2,10 +2,14 @@ package com.boardgo.domain.user.service;
 
 import com.boardgo.domain.user.controller.request.EmailRequest;
 import com.boardgo.domain.user.controller.request.NickNameRequest;
+import com.boardgo.domain.user.entity.UserInfoEntity;
 import com.boardgo.domain.user.service.response.OtherPersonalInfoResponse;
 import com.boardgo.domain.user.service.response.UserPersonalInfoResponse;
 
 public interface UserQueryUseCase {
+
+    UserInfoEntity getById(Long id);
+
     void existEmail(EmailRequest emailRequest);
 
     void existNickName(NickNameRequest nickNameRequest);
