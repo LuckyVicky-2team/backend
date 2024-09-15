826dffcb8a80dbb61c084227c3e1e3b6e211782e
yeahdy <yejine98@naver.com>
2024-09-15 15:11:45 +0900

    Refactor: UserCommand 메소드 퍼사드패턴으로 리팩토링

--- src/main/java/com/boardgo/domain/user/service/UserCommandUseCase.java
+++ src/main/java/com/boardgo/domain/user/service/UserCommandUseCase.java
@@ -1,19 +1,21 @@
 package com.boardgo.domain.user.service;
 
 import com.boardgo.domain.user.controller.request.SignupRequest;
-import com.boardgo.domain.user.controller.request.SocialSignupRequest;
 import com.boardgo.domain.user.controller.request.UserPersonalInfoUpdateRequest;
+import com.boardgo.domain.user.entity.UserInfoEntity;
 import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface UserCommandUseCase {
-    Long signup(SignupRequest signupRequest);
+    Long save(SignupRequest signupRequest);
 
-    Long socialSignup(SocialSignupRequest socialSignupRequest, Long userId);
+    void validateNickNameAndPrTag(String nickName, List<String> prTags);
+
+    UserInfoEntity getUserInfoEntity(Long userId);
+
+    boolean existsUser(String nickname);
 
     void updatePersonalInfo(Long userId, UserPersonalInfoUpdateRequest updateRequest);
 
     void updateProfileImage(Long userId, MultipartFile profileImage);
-
-    void updatePrTags(List<String> changedPrTag, Long userId);
 }
---
+++ src/main/java/com/boardgo/domain/user/service/facade/UserCommandFacadeImpl.java
@@ -0,0 +1,42 @@
+package com.boardgo.domain.user.service.facade;
+
+import com.boardgo.common.exception.DuplicateException;
+import com.boardgo.domain.user.controller.request.SignupRequest;
+import com.boardgo.domain.user.controller.request.SocialSignupRequest;
+import com.boardgo.domain.user.entity.UserInfoEntity;
+import com.boardgo.domain.user.service.UserCommandUseCase;
+import com.boardgo.domain.user.service.UserPrTagCommandUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserCommandFacadeImpl implements UserCommandFacade {
+
+    private final UserCommandUseCase userCommandUseCase;
+    private final UserPrTagCommandUseCase userPrTagCommandUseCase;
+
+    @Override
+    public Long signup(SignupRequest signupRequest) {
+        userCommandUseCase.validateNickNameAndPrTag(
+                signupRequest.nickName(), signupRequest.prTags());
+        // TODO. 약관동의 저장
+        Long userId = userCommandUseCase.save(signupRequest);
+        userPrTagCommandUseCase.bulkInsertPrTags(signupRequest.prTags(), userId);
+        return userId;
+    }
+
+    @Override
+    public Long socialSignup(SocialSignupRequest signupRequest, Long userId) {
+        UserInfoEntity userInfoEntity = userCommandUseCase.getUserInfoEntity(userId);
+        if (userCommandUseCase.existsUser(signupRequest.nickName())) {
+            throw new DuplicateException("중복된 닉네임입니다.");
+        }
+        userCommandUseCase.validateNickNameAndPrTag(
+                signupRequest.nickName(), signupRequest.prTags());
+        // TODO. 약관동의 저장
+        userInfoEntity.updateNickname(signupRequest.nickName());
+        userPrTagCommandUseCase.bulkInsertPrTags(signupRequest.prTags(), userInfoEntity.getId());
+        return userInfoEntity.getId();
+    }
+}
