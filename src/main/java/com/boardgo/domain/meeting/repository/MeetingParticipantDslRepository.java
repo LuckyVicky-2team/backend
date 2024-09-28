64d072fd7a034a5f88583bab7550f2b4b8efbec1
ambosing <ambosing_@naver.com>
2024-09-28 21:22:25 +0900

    Feat: 참여자 목록 조회 구현
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/main/java/com/boardgo/domain/meeting/repository/MeetingParticipantDslRepository.java
+++ src/main/java/com/boardgo/domain/meeting/repository/MeetingParticipantDslRepository.java
@@ -1,9 +1,13 @@
 package com.boardgo.domain.meeting.repository;
 
-import com.boardgo.domain.meeting.repository.projection.ReviewMeetingParticipantsProjection;
 import java.util.List;
+
+import com.boardgo.domain.meeting.repository.projection.ReviewMeetingParticipantsProjection;
+import com.boardgo.domain.user.repository.projection.UserParticipantProjection;
 
 public interface MeetingParticipantDslRepository {
     List<ReviewMeetingParticipantsProjection> findReviewMeetingParticipants(
             List<Long> revieweeIds, Long meetingId);
+
+    List<UserParticipantProjection> findParticipantListByMeetingId(Long meetingId);
 }
--- src/main/java/com/boardgo/domain/meeting/service/MeetingParticipantQueryServiceV1.java
+++ src/main/java/com/boardgo/domain/meeting/service/MeetingParticipantQueryServiceV1.java
@@ -2,16 +2,22 @@ package com.boardgo.domain.meeting.service;
 
 import static com.boardgo.domain.meeting.entity.enums.ParticipantType.*;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.boardgo.common.utils.SecurityUtils;
+import com.boardgo.domain.mapper.MeetingParticipantMapper;
 import com.boardgo.domain.meeting.entity.MeetingParticipantEntity;
 import com.boardgo.domain.meeting.entity.enums.ParticipantType;
 import com.boardgo.domain.meeting.repository.MeetingParticipantRepository;
 import com.boardgo.domain.meeting.service.response.ParticipantOutResponse;
-import java.util.List;
-import java.util.Optional;
+import com.boardgo.domain.meeting.service.response.UserParticipantResponse;
+import com.boardgo.domain.user.repository.projection.UserParticipantProjection;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
@@ -19,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MeetingParticipantQueryServiceV1 implements MeetingParticipantQueryUseCase {
 
     private final MeetingParticipantRepository meetingParticipantRepository;
+    private final MeetingParticipantMapper meetingParticipantMapper;
 
     @Override
     public ParticipantOutResponse getOutState(Long meetingId) {
@@ -36,5 +43,12 @@ public class MeetingParticipantQueryServiceV1 implements MeetingParticipantQuery
     public int getMeetingCount(Long userId) {
         return meetingParticipantRepository.countByTypeAndUserInfoId(
                 List.of(LEADER, PARTICIPANT), userId);
+    }
+
+    @Override
+    public List<UserParticipantResponse> findByMeetingId(Long meetingId) {
+        List<UserParticipantProjection> projectionList = meetingParticipantRepository.findParticipantListByMeetingId(
+            meetingId);
+        return projectionList.stream().map(meetingParticipantMapper::toUserParticipantResponse).toList();
     }
 }
