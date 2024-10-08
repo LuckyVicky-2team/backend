c643d40e23ae4dcbeb2f77cded107681b5d2a831
yeahdy <yejine98@naver.com>
2024-10-08 14:44:52 +0900

    Feat: FCM 환경셋팅

---
+++ src/main/java/fcm/FcmInitializer.java
@@ -0,0 +1,36 @@
+package fcm;
+
+import com.boardgo.common.exception.FcmException;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class FcmInitializer {
+    @Value("${firebase.key-path}")
+    private String KEY_PATH;
+
+    @PostConstruct
+    public void initializeFirebase() {
+        try {
+            InputStream refreshToken = new ClassPathResource(KEY_PATH).getInputStream();
+
+            FirebaseOptions options =
+                    FirebaseOptions.builder()
+                            .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                            .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            throw new FcmException(e.getMessage());
+        }
+    }
+}
