559b4504965b6ab1c898651f335d48b2be6aa1e6
yeahdy <yejine98@naver.com>
2024-10-17 15:42:25 +0900

    Fix: 푸시알림전송 로직 퍼사드 > 스케줄러 job 으로 이동

--- src/main/java/com/boardgo/domain/notification/service/facade/NotificationCommandFacade.java
+++
@@ -1,5 +0,0 @@
-package com.boardgo.domain.notification.service.facade;
-
-public interface NotificationCommandFacade {
-    void sendNotificationPush();
-}
--- src/main/java/com/boardgo/schedule/job/SendPushJob.java
+++ src/main/java/com/boardgo/schedule/job/SendPushJob.java
@@ -1,19 +1,52 @@
 package com.boardgo.schedule.job;
 
-import com.boardgo.domain.notification.service.facade.NotificationCommandFacade;
+import com.boardgo.common.exception.FcmException;
+import com.boardgo.domain.notification.service.NotificationCommandUseCase;
+import com.boardgo.domain.notification.service.NotificationQueryUseCase;
+import com.boardgo.domain.notification.service.response.NotificationPushResponse;
+import com.boardgo.fcm.request.FcmMessageSendRequest;
+import com.boardgo.fcm.service.FcmService;
+import java.util.List;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 
 @DisallowConcurrentExecution
 public class SendPushJob implements Job {
 
-    @Autowired private NotificationCommandFacade notificationCommandFacade;
+    @Autowired private NotificationQueryUseCase notificationQueryUseCase;
+    @Autowired private NotificationCommandUseCase notificationCommandUseCase;
+    @Autowired private FcmService fcmService;
 
+    @Async
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        notificationCommandFacade.sendNotificationPush();
+        List<NotificationPushResponse> notificationPushList =
+                notificationQueryUseCase.getNotificationPushList();
+        if (notificationPushList.isEmpty()) {
+            return;
+        }
+        notificationPushList.forEach(
+                push -> {
+                    String fcmResult = "";
+                    try {
+                        fcmResult =
+                                fcmService.sendFcmMessage(
+                                        new FcmMessageSendRequest(
+                                                push.token(),
+                                                push.title(),
+                                                push.content(),
+                                                push.pathUrl()));
+                        // TODO DB 복합인덱스 키 생성
+                        notificationCommandUseCase.saveNotificationResult(
+                                push.notificationId(), fcmResult);
+                    } catch (FcmException fe) {
+                        fcmResult = fe.getMessage();
+                        // TODO fcm 푸시 실패 시 저장
+                    }
+                });
     }
 }
