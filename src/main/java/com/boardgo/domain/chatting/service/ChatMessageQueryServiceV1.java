fa684bb7295f8c9ec46a18241ebf11dbf42c23cd
ambosing <ambosing_@naver.com>
2024-10-21 22:43:24 +0900

    Feat: 디버깅을 위한 로그
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/main/java/com/boardgo/domain/chatting/service/ChatMessageQueryServiceV1.java
+++ src/main/java/com/boardgo/domain/chatting/service/ChatMessageQueryServiceV1.java
@@ -1,12 +1,15 @@
 package com.boardgo.domain.chatting.service;
 
 import com.boardgo.domain.chatting.repository.ChatRepository;
+import com.boardgo.domain.chatting.repository.projection.LatestMessageProjection;
 import com.boardgo.domain.chatting.service.response.LatestMessageResponse;
 import com.boardgo.domain.mapper.ChatMapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatMessageQueryServiceV1 implements ChatMessageQueryUseCase {
@@ -16,7 +19,14 @@ public class ChatMessageQueryServiceV1 implements ChatMessageQueryUseCase {
 
     @Override
     public List<LatestMessageResponse> getLast(List<Long> roomIdList) {
-        return chatMapper.toLatestMessageResponse(
-                chatRepository.findLatestMessagesByRoomIds(roomIdList));
+        List<LatestMessageProjection> result =
+                chatRepository.findLatestMessagesByRoomIds(roomIdList);
+        for (LatestMessageProjection latestMessageProjection : result) {
+            log.info("latestMessageProjection.roomId = {}", latestMessageProjection.roomId());
+            log.info(
+                    "latestMessageProjection.latestMessage.Content = {}",
+                    latestMessageProjection.latestMessage().getContent());
+        }
+        return chatMapper.toLatestMessageResponse(result);
     }
 }
