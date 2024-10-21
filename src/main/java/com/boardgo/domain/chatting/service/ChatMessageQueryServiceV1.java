4ab0ee003a3c3b5d51b2bc168795fa3462b632ef
ambosing <ambosing_@naver.com>
2024-10-21 23:31:35 +0900

    Feat: 채팅방 이전 내용 조회
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/main/java/com/boardgo/domain/chatting/service/ChatMessageQueryServiceV1.java
+++ src/main/java/com/boardgo/domain/chatting/service/ChatMessageQueryServiceV1.java
@@ -1,12 +1,19 @@
 package com.boardgo.domain.chatting.service;
 
+import com.boardgo.domain.chatting.entity.ChatMessage;
 import com.boardgo.domain.chatting.repository.ChatRepository;
 import com.boardgo.domain.chatting.repository.projection.LatestMessageProjection;
+import com.boardgo.domain.chatting.service.response.ChatMessageResponse;
 import com.boardgo.domain.chatting.service.response.LatestMessageResponse;
 import com.boardgo.domain.mapper.ChatMapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -28,5 +35,15 @@ public class ChatMessageQueryServiceV1 implements ChatMessageQueryUseCase {
                     latestMessageProjection.latestMessage().getContent());
         }
         return chatMapper.toLatestMessageResponse(result);
+    }
+
+    @Override
+    public Page<ChatMessageResponse> getChatHistory(Long chatRoomId, int page) {
+        Pageable pageable = PageRequest.of(page, 20, Sort.by(Sort.Direction.DESC, "sendDatetime"));
+        Page<ChatMessage> result = chatRepository.findByRoomId(chatRoomId, pageable);
+        return new PageImpl<>(
+                chatMapper.toChatMessageResponseList(result.getContent()),
+                pageable,
+                result.getTotalElements());
     }
 }
