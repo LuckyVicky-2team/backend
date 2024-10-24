f358b78fc3ed9c97d43a1ab872d64557f4f542fb
yeahdy <yejine98@naver.com>
2024-10-24 16:58:34 +0900

    Feat: 알림발송 시 알림약관동의 및 푸시설정 비활성화일 경우 발송안하기 로직 추가

--- src/test/java/com/boardgo/integration/notification/service/UserNotificationSettingQueryServiceV1Test.java
+++ src/test/java/com/boardgo/integration/notification/service/UserNotificationSettingQueryServiceV1Test.java
@@ -27,14 +27,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserNotificationSettingQueryServiceV1Test extends IntegrationTestSupport {
 
-    @Autowired
-    UserNotificationSettingQueryUseCase userNotificationSettingQueryUseCase;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    NotificationSettingRepository notificationSettingRepository;
-    @Autowired
-    UserNotificationSettingRepository userNotificationSettingRepository;
+    @Autowired UserNotificationSettingQueryUseCase userNotificationSettingQueryUseCase;
+    @Autowired UserRepository userRepository;
+    @Autowired NotificationSettingRepository notificationSettingRepository;
+    @Autowired UserNotificationSettingRepository userNotificationSettingRepository;
     private final List<NotificationSettingEntity> notificationSettings = getNotificationSettings();
 
     @BeforeEach
@@ -49,15 +45,7 @@ public class UserNotificationSettingQueryServiceV1Test extends IntegrationTestSu
         UserInfoEntity user = userInfoEntityData("user1@naver.com", "user1").build();
         userRepository.save(user);
 
-        notificationSettings.forEach(
-                notificationSettingEntity -> {
-                    userNotificationSettingRepository.save(
-                            UserNotificationSettingEntity.builder()
-                                    .userInfoId(user.getId())
-                                    .notificationSetting(notificationSettingEntity)
-                                    .isAgreed(true)
-                                    .build());
-                });
+        notificationSettings.forEach(notificationSettingEntity -> {});
 
         // when
         List<UserNotificationSettingResponse> userNotificationSettingsList =
@@ -86,9 +74,9 @@ public class UserNotificationSettingQueryServiceV1Test extends IntegrationTestSu
         // when
         // then
         assertThatThrownBy(
-                () ->
-                        userNotificationSettingQueryUseCase.getUserNotificationSettingsList(
-                                user.getId()))
+                        () ->
+                                userNotificationSettingQueryUseCase.getUserNotificationSettingsList(
+                                        user.getId()))
                 .isInstanceOf(CustomIllegalArgumentException.class)
                 .hasMessageContaining("회원의 알림설정이 존재하지 않습니다");
     }
@@ -97,7 +85,7 @@ public class UserNotificationSettingQueryServiceV1Test extends IntegrationTestSu
     @DisplayName("알림 항목 별로 회원의 알림설정 활성화 유무를 확인할 수 있다")
     @EnumSource(MessageType.class)
     void 알림_항목_별로_회원의_알림설정_활성화_유무를_확인할_수_있다(MessageType messageType) {
-        //given
+        // given
         UserInfoEntity user = userInfoEntityData("user1@naver.com", "user1").build();
         userRepository.save(user);
 
@@ -111,13 +99,14 @@ public class UserNotificationSettingQueryServiceV1Test extends IntegrationTestSu
                                     .build());
                 });
 
-        //when
+        // when
         UserNotificationSettingEntity userNotificationSetting =
-                userNotificationSettingQueryUseCase.getUserNotificationSetting(user.getId(), messageType);
+                userNotificationSettingQueryUseCase.getUserNotificationSetting(
+                        user.getId(), messageType);
 
-        //then
-        assertThat(userNotificationSetting.getNotificationSetting().getMessageType()).isEqualTo(messageType);
+        // then
+        assertThat(userNotificationSetting.getNotificationSetting().getMessageType())
+                .isEqualTo(messageType);
         assertThat(userNotificationSetting.getIsAgreed()).isNotNull().isInstanceOf(Boolean.class);
     }
-
 }
