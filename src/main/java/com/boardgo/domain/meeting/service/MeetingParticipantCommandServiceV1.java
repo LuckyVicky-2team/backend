92cd75e563d4ccbd3dcb46a25ef69dbc13d39716
yeahdy <yejine98@naver.com>
2025-01-08 15:11:30 +0900

    Fix: 모임 참여 이슈 수정
    모임 참여 시 모임 상태가 진행중이 아닐 경우 참여하지 못하도록 수정

--- src/main/java/com/boardgo/domain/meeting/service/MeetingParticipantCommandServiceV1.java
+++ src/main/java/com/boardgo/domain/meeting/service/MeetingParticipantCommandServiceV1.java
@@ -1,21 +1,18 @@
 package com.boardgo.domain.meeting.service;
 
+import static com.boardgo.domain.meeting.entity.enums.MeetingState.PROGRESS;
 import static com.boardgo.domain.meeting.entity.enums.ParticipantType.OUT;
 
 import com.boardgo.common.exception.CustomIllegalArgumentException;
 import com.boardgo.common.exception.CustomNullPointException;
 import com.boardgo.domain.mapper.MeetingParticipantMapper;
-import com.boardgo.domain.mapper.MeetingParticipateWaitingMapper;
 import com.boardgo.domain.meeting.controller.request.MeetingParticipateRequest;
 import com.boardgo.domain.meeting.entity.MeetingEntity;
 import com.boardgo.domain.meeting.entity.MeetingParticipantEntity;
 import com.boardgo.domain.meeting.entity.MeetingParticipantSubEntity;
-import com.boardgo.domain.meeting.entity.MeetingParticipateWaitingEntity;
-import com.boardgo.domain.meeting.entity.enums.AcceptState;
 import com.boardgo.domain.meeting.entity.enums.ParticipantType;
 import com.boardgo.domain.meeting.repository.MeetingParticipantRepository;
 import com.boardgo.domain.meeting.repository.MeetingParticipantSubRepository;
-import com.boardgo.domain.meeting.repository.MeetingParticipateWaitingRepository;
 import com.boardgo.domain.meeting.repository.MeetingRepository;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
@@ -30,9 +27,7 @@ public class MeetingParticipantCommandServiceV1 implements MeetingParticipantCom
     private final MeetingParticipantRepository meetingParticipantRepository;
     private final MeetingRepository meetingRepository;
     private final MeetingParticipantSubRepository meetingParticipantSubRepository;
-    private final MeetingParticipateWaitingRepository meetingParticipateWaitingRepository;
     private final MeetingParticipantMapper meetingParticipantMapper;
-    private final MeetingParticipateWaitingMapper meetingParticipateWaitingMapper;
 
     @Override
     public void participateMeeting(MeetingParticipateRequest participateRequest, Long userId) {
@@ -42,20 +37,10 @@ public class MeetingParticipantCommandServiceV1 implements MeetingParticipantCom
                         .orElseThrow(() -> new CustomNullPointException("모임이 존재하지 않습니다"));
         validateParticipateMeeting(meetingEntity, userId);
 
-        switch (meetingEntity.getType()) {
-            case FREE -> {
-                MeetingParticipantEntity participant =
-                        meetingParticipantMapper.toMeetingParticipantEntity(
-                                meetingEntity.getId(), userId, ParticipantType.PARTICIPANT);
-                meetingParticipantRepository.save(participant);
-            }
-            case ACCEPT -> {
-                MeetingParticipateWaitingEntity participateWaitingEntity =
-                        meetingParticipateWaitingMapper.toMeetingParticipateWaitingEntity(
-                                meetingEntity.getId(), userId, AcceptState.WAIT);
-                meetingParticipateWaitingRepository.save(participateWaitingEntity);
-            }
-        }
+        MeetingParticipantEntity participant =
+                meetingParticipantMapper.toMeetingParticipantEntity(
+                        meetingEntity.getId(), userId, ParticipantType.PARTICIPANT);
+        meetingParticipantRepository.save(participant);
     }
 
     @Override
@@ -80,6 +65,9 @@ public class MeetingParticipantCommandServiceV1 implements MeetingParticipantCom
 
     private void validateParticipateMeeting(MeetingEntity meetingEntity, Long userId) {
         meetingEntity.isAfterMeeting();
+        if (!meetingEntity.isSameState(PROGRESS)) {
+            throw new CustomIllegalArgumentException("모임 중인 상태만 참여할 수 있습니다.");
+        }
         if (meetingParticipantRepository.existsByUserInfoIdAndMeetingId(
                 userId, meetingEntity.getId())) {
             throw new CustomIllegalArgumentException("이미 참여된 모임 입니다");
